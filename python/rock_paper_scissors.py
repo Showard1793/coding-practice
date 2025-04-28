7369a810cbaa82a93362a52db0c03aec1db0ae44
showard1793 <Showard1793@gmail.com>
2025-04-27 21:05:28 -0500

    finished rock paper scissors

--- python/rock_paper_scissors.py
+++ python/rock_paper_scissors.py
@@ -2,15 +2,30 @@ import random
 
 choice = ["Rock","Paper","Scissor"]
 
-player_choice = int(input("What will you choose?\n 1)Rock\n 2)Paper\n 3)Scissor"))
+player_choice = int(input("What will you choose?\n 1)Rock\n 2)Paper\n 3)Scissor \n"))
 opponent_choice = (random.choice(choice))
 
 #Player chooses Rock
 if player_choice == 1 and opponent_choice == "Scissor":
-    print("Rock beats Scissor, you win!")
+    print("Computer chose " + opponent_choice + ". Rock beats Scissor, you win!")
 if player_choice == 1 and opponent_choice == "Rock":
-    print("Tie")
+    print("Computer chose " + opponent_choice + ". Tie")
 if player_choice == 1 and opponent_choice == "Paper":
-    print("Paper beats Rock, you lose!")
+    print("Computer chose " + opponent_choice + ". Paper beats Rock, you lose!")
 
 #Player chooses Paper
+if player_choice == 2 and opponent_choice == "Scissor":
+    print("Computer chose " + opponent_choice + ". Scissor beats Paper, you lose!")
+if player_choice == 2 and opponent_choice == "Rock":
+    print("Computer chose " + opponent_choice + ". Paper beats Rock, you win!")
+if player_choice == 2 and opponent_choice == "Paper":
+    print("Computer chose " + opponent_choice + ". Tie")
+
+#Player chooses Scissor
+if player_choice == 3 and opponent_choice == "Scissor":
+    print("Computer chose " + opponent_choice + ". Tie")
+if player_choice == 3 and opponent_choice == "Rock":
+    print("Computer chose " + opponent_choice + ". Rock beats Scissor, you lose!")
+if player_choice == 3 and opponent_choice == "Paper":
+    print("Computer chose " + opponent_choice + ". Scissor beats Paper, you win!")
+
