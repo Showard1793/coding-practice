e4054b1b52c8a056a16746abe9ce8ac36fd08496
showard1793 <Showard1793@gmail.com>
2025-05-19 21:47:47 -0500

    Updated

--- python/dice_roll.py
+++ python/dice_roll.py
@@ -1,9 +1,28 @@
 import random
 
-roll = random.randint(1,6)
-print(roll)
+player1 = input("Enter Player 1's name:\n")
+player2 = input("Enter Player 2's name:\n")
 
-options = ["cat","dog","snake"]
 
-print(random.choice(options))
+
+def roll_dice():
+    return random.randint(1,6)
+
+
+def play():
+    player1roll = roll_dice()
+    player2roll = roll_dice()
+    print(player1, "rolled a",str(player1roll)+",", "and",player2,"rolled a",player2roll)
+    if player1roll == player2roll:
+        print("It's a tie!")
+    elif player1roll > player2roll:
+        print(player1,"wins!")
+    else:
+        print(player2,"wins!")
+
+play()
+
+# options = ["cat","dog","snake"]
+
+# print(random.choice(options))
 
